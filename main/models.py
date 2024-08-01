96e4c132887ec7f26cc773dc6cbda7210d424896
River <riveriswild@icloud.com>
2024-08-02 00:10:53 +0300

    Refactor models

--- main/models.py
+++ main/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 
+
 class Location(models.Model):
-    ''' Tree-like location structure within the house'''
+    """Tree-like location structure within the house"""
+
     name = models.CharField(max_length=256)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, related_name='children')
+    parent = models.ForeignKey(
+        "self", on_delete=models.CASCADE, blank=True, null=True, related_name="children"
+    )
 
     def __str__(self):
         return self.name
 
+
 class GeneralItem(models.Model):
-    ''' Describes a general item in inventory'''
+    """Describes a general item in inventory"""
+
     purchased_at = models.DateTimeField(auto_now_add=True)
     purchase_place = models.CharField(max_length=1024, blank=True, null=True)
     photo = models.ImageField(upload_to="images/")
